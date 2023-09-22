ae3fa970a3acfd4c23a7197d74c2fabcdd6f7d1a
L1ghtzin <lightzin021@gmail.com>
2023-09-22 10:00:31 -0300

    sdm632-common: Update extract-files and setup-makefiles

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,6 +8,9 @@
 
 set -e
 
+DEVICE_COMMON=sdm632-common
+VENDOR=motorola
+
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
