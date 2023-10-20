11ad8ee5804e5191288b635ef6c6df950a70abf6
L1ghtzin <lightzin021@gmail.com>
2023-10-20 14:00:05 -0300

    sdm632-common: Update Thermal Blobs from LA.UM.10.6.2.r1-02500-89xx.0
    Change-Id: I00f7eb754bc25bc70693c316a87b48288604ebe8

--- common.mk
+++ common.mk
@@ -450,6 +450,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@1.0-service
+    
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf        
 
 # Touchscreen
 PRODUCT_PACKAGES += \
