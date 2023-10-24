f1d78359e63048a52daa452386c2ff692c319e88
L1ghtzin <lightzin021@gmail.com>
2023-10-23 22:49:03 -0300

    sdm632-common: Patch libs to load libprotobuf-v29
    22:31:42.974  1856  1856 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.drm@1.2-service.widevine": cannot locate symbol "_ZN6google8protobuf8internal13empty_string_E" referenced by "/vendor/lib64/libwvhidl.so"...

--- extract-files.sh
+++ extract-files.sh
@@ -105,6 +105,13 @@ function blob_fixup() {
         vendor/lib/soundfx/libspeakerbundle.so | vendor/lib/sensors.rp.so | vendor/lib64/sensors.rp.so)
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
             ;;
+        # Use Protobuf-V29
+        vendor/lib64/libsettings.so | vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so"
+            ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so"
+            ;;
     esac
 }
 
